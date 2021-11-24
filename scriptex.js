0d4f5f6a57c1aff1aad912b7aaddef82b867ca2e
igorkawai1 <92556371+igorkawai1@users.noreply.github.com>
2021-11-24 15:35:53 -0300

    Mudança no index

--- scriptex.js
+++ scriptex.js
@@ -3,7 +3,6 @@ function carregar() {
     var img = window.document.getElementById('imagem')  
     var data = new Date()
     var hora = data.getHours()
-    var hora = 21
     msg.innerHTML= `Agora são ${hora} horas.</br>`
     if (hora >= 0 && hora < 12) {
         msg.innerText += `Bom dia!`
